273895850dd689076389d0a8087d90b428b2ad19
Ustazoda <sohibjonustaboyev@gmail.com>
2026-07-31 13:48:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ def is_sunday() -> bool:
 # ==========================================================
 # 🔑 SOZLAMALAR
 # ==========================================================
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8707986524:AAFWdfPP2hovaeDOBayV-5uR7jKGP_bzdmI")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID", "-1003993511736"))
 JARIMALAR_THREAD_ID = int(os.getenv("JARIMALAR_THREAD_ID", "55"))
 ISHGA_KELISH_THREAD_ID = int(os.getenv("ISHGA_KELISH_THREAD_ID", "1"))
